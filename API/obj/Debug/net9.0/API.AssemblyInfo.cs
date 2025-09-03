d53fb12ea2535daf52cb4411d61bb9a8862a73b8
Berkay Dagitan <stackzyy@gmail.com>
2025-09-03 21:29:04 +0300

    Client klasöründe değişiklikler yapıldı

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a9644a0d7d9f36effc0f2d41e6b1a392708524")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec12d64758021c42eebd91e8da77708bdeb8e2ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
