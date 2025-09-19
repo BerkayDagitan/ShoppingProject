7ab36ce6f1c31ec5fbf042cb08c82980a1ad7c11
Berkay Dagitan <stackzyy@gmail.com>
2025-09-20 01:46:53 +0300

    20.09.2025

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b88a23f27f4061d8f7dc151f1f00167999149f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd2a5eebefb129722f471982acb290b67da34af")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
